6158a3e39fd0592f6eaa1f0afd85d3f64bff6ee5
PauloMatiasxvs <155742553+PauloMatiasxvs@users.noreply.github.com>
2025-01-16 12:30:52 -0300

    atualizando

--- Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0332d4a3071685ff62ef44a03ebfadc4f9e72208")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
